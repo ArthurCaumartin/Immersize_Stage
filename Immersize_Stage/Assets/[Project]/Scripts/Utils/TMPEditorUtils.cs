ad6399be0d2826cf8cae8d1429298d2ef1b326f2
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-06-04 15:48:05 +0200

    Add utils class to force serialize tmp .text

---
+++ Immersize_Stage/Assets/[Project]/Scripts/Utils/TMPEditorUtils.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public static class TMPEditorUtils
+{
+    public static void SetSerializeProperty(this TextMeshProUGUI textMesh, string value)
+    {
+        SerializedObject serializedObj = new SerializedObject(textMesh);
+        SerializedProperty property = serializedObj.FindProperty("m_text");
+
+        if (property != null)
+        {
+            Debug.Log("Property Find : " + property.name);
+            property.stringValue = value;
+            serializedObj.ApplyModifiedProperties();
+
+            EditorUtility.SetDirty(textMesh);
+            EditorSceneManager.MarkSceneDirty(textMesh.gameObject.scene);
+        }
+        else
+        {
+            Debug.Log("No property find");
+        }
+    }
+}
